5dadb12c0745baa385156e77f8f544ce6f77af99
Alec Larson <1925840+aleclarson@users.noreply.github.com>
2025-02-18 21:01:43 -0500

    fix: copy property descriptors when forwarding hattip context

--- src/common.ts
+++ src/common.ts
@@ -1,6 +1,5 @@
 import type { RequestContext } from '@hattip/compose'
 import * as crossws from 'crossws'
-import { omit } from 'radashi'
 import type { Hooks, ResolveHooks } from './core.js'
 import type { WebSocketAdapterOptions } from './index.js'
 
@@ -27,13 +26,21 @@ const ignoredContextKeys = [
   'waitUntil',
 ] as const
 
-export function forwardHattipContext(target: any, context: RequestContext) {
-  if (target.context) {
-    Object.assign(target.context, omit(context, ignoredContextKeys))
-  } else {
+export function forwardHattipContext(target: any, source: RequestContext) {
+  const context: Record<string, unknown> = target.context ?? {}
+  for (const name of Object.getOwnPropertyNames(source)) {
+    if (!ignoredContextKeys.includes(name as any)) {
+      Object.defineProperty(
+        context,
+        name,
+        Object.getOwnPropertyDescriptor(source, name)!
+      )
+    }
+  }
+  if (!target.context) {
     Object.defineProperty(target, 'context', {
       enumerable: true,
-      value: omit(context, ignoredContextKeys),
+      value: context,
     })
   }
 }
